64faf549264e4576c62fd2a99d6fee76f2f2412e
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-06 15:43:39 +0000

    Add div with anchors to Sidebar js file

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+    return <div>
+        <a href="#." target= "_blank">My Photos</a>
+        <a href="#." target= "_blank">My Illustrations</a>
+        <a href="#." target= "_blank">My Paintings</a>
+        
+    </div>
+}
+
+
+
+
+
+export default Sidebar
