83d24af2587f01b720146075ec0d70d6c34e46d3
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-18 16:33:24 +0000

    Add post item child file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className={css.App}>
       <Sidebar />
       <NavBarForm />
+      <Content />
     </div>
   );
 }
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 import css from "./css/Content.module.css"
-import {savedPosts} from postMessage.json
+import { savedPosts } from '../posts.json'
+import PostItem from './PostItem'
 
 export class Content extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
-      <div className= {css.Content}>
-        <div className= {css.TitleBar}>
-            <h1>My Posts</h1>
+      <div className={css.Content}>
+
+        <div className={css.TitleBar}>
+
+          <h1>My Photos</h1>
         </div>
+
         <div className={css.SearchResults}>
-
-
-
-
-            Content goes here
+          <PostItem savedPosts={savedPosts} />
         </div>
       </div>
     )
