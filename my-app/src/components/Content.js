3cd23caef572b523e32080c9c7c179d4cf1be8bc
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-26 15:22:53 +0000

    Add search posts feature

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,31 +8,54 @@ export class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoaded: false
+      isLoaded: false,
+      posts: [],
     }
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        isLoaded: true
-      })
-    }, 2000)
+  componentDidMount() {  
+    
+      setTimeout(() => {
+        this.setState({
+          isLoaded: true,
+          posts: savedPosts,
+        })
+      }, 2000)
   }
+
+  handleChange = (e) => {
+    const name = e.target.value.toLowerCase();
+    const filteredPosts = savedPosts.filter((post)=>{
+        return post.name.toLowerCase().includes(name);
+    })    
+    this.setState({
+        posts: filteredPosts
+    })
+}
 
   render() {
     return (
       <div className={css.Content}>
 
         <div className={css.TitleBar}>
-
           <h1>My Photos</h1>
         </div>
+
+        <form>
+          <label htmlFor='searchInput'>Search:  </label>
+          <input             
+            type="search"
+            id = "searchInput" 
+            placeholder='By Author'
+            onChange={(e) => this.handleChange(e)}
+          />
+          <h4>posts found: {this.state.posts.length}</h4>
+        </form>
 
         <div className={css.SearchResults}>
           {
             this.state.isLoaded ?
-              <PostItem savedPosts={savedPosts} />
+              <PostItem savedPosts={this.state.posts} />
               : <Loader />
           }
         </div>
