f38f6700383e7152f6e3ff2ee67d8377f862bbfd
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-27 17:07:03 +0000

    Change content to content hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,8 @@ import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
+// import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <div className={css.App}>
       <Sidebar />
       <NavBarForm />
-      <Content />
+      <ContentHooks />
     </div>
   );
 }
