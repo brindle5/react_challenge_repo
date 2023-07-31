8e7ad4388e3d27a8d8b8f14dd36792d7e9c32e58
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-31 11:09:56 +0000

    Convert class component to hook

--- my-app/src/components/ContentAPIHooks.js
+++ my-app/src/components/ContentAPIHooks.js
@@ -1,48 +1,39 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from 'react'
 import css from "./css/Content.module.css";
 import PostItemAPI from "./PostItemAPI";
 import Loader from "./Loader";
 import axios from "axios";
 import API_KEY from "../secrets";
 
-export class ContentAPI extends Component {
-    constructor(props) {
-        super(props)
+function ContentAPIHooks() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [savedPosts, setSavedPosts] = useState([]);
 
-        this.state = {
-            isLoaded: false,
-            posts: [],
-            savedPosts: [],
-        }
-    }
-
-    componentDidMount() {
-        this.fetchImages();
-    }
-
-    async fetchImages() {
-        const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100&safesearch=true&editors_choice=true&orientation=horizontal`);
-        const fetchedPosts = response.data.hits;
-
-        this.setState({
-            isLoaded: true,
-            posts: fetchedPosts,
-            savedPosts: fetchedPosts,
-        })
-    }
+    useEffect(() => {
+          fetchImages()
+        }, []);
     
+
+    const fetchImages = async () => {
+        const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100&safesearch=true&editors_choice=true&orientation=horizontal`);
+        const fetchedPosts = response.data.hits;   
+        
+        setIsLoaded(true);
+        setPosts(fetchedPosts);
+        setSavedPosts(fetchedPosts);
+    }
+
+
     handleChange = (e) => {
         const name = e.target.value.toLowerCase();
-        const filteredPosts = this.state.savedPosts.filter((post)=>{
+        const filteredPosts = savedPosts.filter((post)=>{
             return post.user.toLowerCase().includes(name);
         })
         
-        this.setState({
-            posts: filteredPosts
-        })
+        setPosts(filteredPosts);
     }
 
-    render() {
         return (
             <div className={css.Content}>
                 
@@ -54,23 +45,22 @@ export class ContentAPI extends Component {
                         type="search" 
                         id="searchinput" 
                         placeholder="By Author"
-                        onChange={(e) => this.handleChange(e)}
+                        onChange={(e) => handleChange(e)}
                         />
-                        <h4>posts found {this.state.posts.length}</h4>
+                        <h4>posts found {posts.length}</h4>
                     </form>
                 </div>
 
                 <div className={css.SearchResults}>
                     {
-                        this.state.isLoaded ?
-                        <PostItemAPI savedPosts={this.state.posts} />
+                        isLoaded ?
+                        <PostItemAPI savedPosts={posts} />
                         : <Loader />
                     }
-
                 </div>
             </div>
         )
-    }
-}
+    }  
 
-export default ContentAPI
+
+export default ContentAPIHooks
